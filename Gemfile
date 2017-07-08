bcc2287028f97542af7b2ab7c7f349f47c0936d1
Itoh Kaori <k197781@kansai-u.ac.jp>
2017-07-08 17:13:46 +0900

    make ranking

--- Gemfile
+++ Gemfile
@@ -15,6 +15,7 @@ gem 'jquery-rails', '4.3.1'
 gem 'turbolinks',   '5.0.1'
 gem 'jbuilder',     '2.6.1'
 gem 'devise'
+gem 'redis'
 
 group :development, :test do
   gem 'sqlite3', '1.3.13'
--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -13,15 +13,22 @@ class NotesController < ApplicationController
   end
 
   def show
-  	@note = Note.find_by(params[:id])
+  	@note = Note.find_by(id: params[:id])
+  	REDIS.zincrby "ranking", 1, @note.id
+    ids = REDIS.zrevrangebyscore "ranking", "+inf", 0, limit: [0, 5]
+    @ranking_articles = ids.map{ |id| Note.find(id) }
+    if @ranking_articles.count < 5
+      adding_articles = Note.order(updated_at: :DESC).where.not(id: ids).limit(5 - @ranking_articles.count)
+      @ranking_articles.concat(adding_articles)
+    end
   end
 
   def edit
-  	@note = Note.find_by(params[:id])
+  	@note = Note.find_by(id: params[:id])
   end
 
   def update
-  	@note = Note.find_by(params[:id])
+  	@note = Note.find_by(id: params[:id])
   	if @note.save
   		redirect_to @note
   	else
@@ -30,7 +37,7 @@ class NotesController < ApplicationController
   end
 
   def destroy
-  	Note.find_by(params[:id]).destroy
+  	Note.find_by(id: params[:id]).destroy
   	redirect_to root_url
   end
 
